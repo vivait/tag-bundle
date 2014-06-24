f94a532b054cc4134072ee865b4d47e01c473dcf
Kiel Goodman <kiel@vivait.co.uk>
2014-06-24 10:29:50 +0100

    Implemented default settings for fpn_tag

--- DependencyInjection/VivaitTagExtension.php
+++ DependencyInjection/VivaitTagExtension.php
@@ -4,6 +4,7 @@ namespace Vivait\TagBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,18 +13,20 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class VivaitTagExtension extends Extension
+class VivaitTagExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
      */
+    public function prepend(ContainerBuilder $container) {
+            $config['model']['tag_class'] = 'Vivait\TagBundle\Entity\Tag';
+            $config['model']['tagging_class'] = 'Vivait\TagBundle\Entity\Tagging';
+            $container->prependExtensionConfig('fpn_tag', $config);
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $container->setParameter('fpn_tag.entity.tag.class', 'Vivait\TagBundle\Entity\Tag');
-        $container->setParameter('fpn_tag.entity.tagging.class', 'Vivait\TagBundle\Entity\Tagging');
-
     }
 }
