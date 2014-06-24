e3c285437f4f3e6d153436749cd978f54bba1a85
Kiel Goodman <kiel@vivait.co.uk>
2014-06-24 10:08:36 +0100

    Initial Commit

---
+++ Entity/Tagging.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Vivait\TagBundle\Entity;
+
+use \FPN\TagBundle\Entity\Tagging as BaseTagging;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\UniqueConstraint;
+
+/**
+ * Vivait\TagBundle\Entity\Tagging
+ *
+ * @ORM\Table(uniqueConstraints={@UniqueConstraint(name="tagging_idx", columns={"tag_id", "resource_type", "resource_id"})})
+ * @ORM\Entity
+ */
+class Tagging extends BaseTagging
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Tag", inversedBy="tagging")
+     * @ORM\JoinColumn(name="tag_id", referencedColumnName="id")
+     **/
+    protected $tag;
+}
---
+++ VivaitTagBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vivait\TagBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VivaitTagBundle extends Bundle
+{
+}
